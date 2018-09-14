b47f5d5fd74b793f50d84f9cafe66ef30dcc253f
mohammed mahmoud <mohammed.95mahmoud@gmail.com>
2018-09-14 18:59:39 +0200

    add base package

---
+++ app/src/main/java/com/mahmoud/mohammed/androidtask/base/BaseFragment.kt
@@ -0,0 +1,10 @@
+package com.mahmoud.mohammed.androidtask.base
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment: Fragment() {
+
+    protected fun navigateToDeliveriesDetailsScreen() {
+
+    }
+}
